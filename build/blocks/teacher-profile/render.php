10700c1d0025ea992d4028d24304ac1a46622b57
Yeison Ramirez <yeisonrazar98@gmail.com>
2025-11-27 18:30:58 -0500

    fix: render.php on block teacher-profile

--- build/blocks/teacher-profile/render.php
+++ build/blocks/teacher-profile/render.php
@@ -127,7 +127,7 @@ $wrapper_attributes = get_block_wrapper_attributes([
                     <?php endif; ?>
 
                     <?php if ($show_title && $user_title): ?>
-                        <p style="margin: 0; font-size: 1.3rem;">
+                        <p style="margin: 0; font-size: 1rem;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                                 class="lucide lucide-graduation-cap-icon lucide-graduation-cap inline-block mr-2">
